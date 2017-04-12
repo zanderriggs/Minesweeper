c8f41bb2950d87b4a67f20880028886ec8706e15
Rig <zander.riggs@gmail.com>
2017-04-12 17:14:14 -0400

    created Visual Studio file for Minesweeper project and wrote notation for the basic class structure that we discussed yesterday.

---
+++ Minesweeper/Minesweeper/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Spaces
+    {
+        //private values here VVV
+
+        //Default constructor
+
+        //Spaces constructor (with user assigned properties)
+
+        //GenerateField() method
+
+        //get sets
+        
+    }
+    class Mine : Spaces
+    {
+        //private values here
+
+        //Default constructor
+
+        //Mine constructor
+
+        //LayMines() - to randomly place mines in field
+
+        //get sets
+    }
+}
