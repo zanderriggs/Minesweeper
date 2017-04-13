ca87c8afb8d90b4ef973a9e010d23b507624a688
Rachel <r.overflow.mail@gmail.com>
2017-04-12 20:51:13 -0400

    added some text in main progam

--- Minesweeper/Minesweeper/Program.cs
+++ Minesweeper/Minesweeper/Program.cs
@@ -10,6 +10,18 @@ namespace Minesweeper
     {
         static void Main(string[] args)
         {
+            string userName;
+            int sideLength;
+
+            Console.WriteLine("Hello there, you think you are brave enough to navigate the minefield?");
+            Console.Write("Enter your name so we know who is trespassing here... ");
+
+            userName = Console.ReadLine();
+
+            Console.WriteLine($"{userName}, what size minefield can you handle?");
+            Console.Write("Enter a number between 3 and 13 to indicate the size of your desired minefield: ");
+
+            sideLength = int.Parse(Console.ReadLine());
         }
     }
 }
