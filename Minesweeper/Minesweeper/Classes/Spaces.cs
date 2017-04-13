a688b72efc3a8773f42291b183c6253bba1864ac
sgarner59 <sgarner@steelcase.com>
2017-04-12 20:53:08 -0400

    Shane's change to Spaces class including addition of Random Number Generator

--- Minesweeper/Minesweeper/Classes/Spaces.cs
+++ Minesweeper/Minesweeper/Classes/Spaces.cs
@@ -6,17 +6,45 @@ using System.Threading.Tasks;
 
 namespace Minesweeper.Classes
 {
-    class Spaces
+    class Spaces 
+    
     {
-        //private values here VVV
+        //private values here
+        private int x = 0;
+        private int y = 0;
 
         //Default constructor
+        public Spaces()
+        {
 
-        //Spaces constructor (with user assigned properties)
+        }
 
-        //GenerateField() method
+        //constructor
+        public Spaces(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
 
         //get sets
+        public int X
+        {
+            get { return x; }
+            set { value = x; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { value = y; }
+        }
 
+        public int PlantMines() // Method
+            {
+                Random r = new Random();
+                for (int i = 0; i < 25; i++)
+                Console.WriteLine(r.Next(1, 50).ToString());
+
+            }
+    
     }
 }
