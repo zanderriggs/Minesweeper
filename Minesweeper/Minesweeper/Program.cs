b1e247cac62f19611181b3124fed5cce524b39f8
Rachel <r.overflow.mail@gmail.com>
2017-04-15 14:50:36 -0400

    more program fleshing out

--- Minesweeper/Minesweeper/Program.cs
+++ Minesweeper/Minesweeper/Program.cs
@@ -7,18 +7,21 @@ using System.Threading.Tasks;
 
 namespace Minesweeper
 {
+    enum xValues { Z, Y, X, W, V, U, T, S, R, Q, P, O, N }
+    enum yValues { A, B, C, D, E, F, G, H, I, J, K, L, M }
+
     class Program
     {
 
 
         static void Main(string[] args)
         {
-            string answer;
+            string finalAnswer;
             string userName;
             int sideLength;
-            string userBool;
             string userChoice;
             string userSpace;
+            
 
             Console.WriteLine("Hello there, you think you are brave enough to navigate the minefield?");
             Console.Write("Enter your name so we know who is trespassing here... ");
@@ -36,10 +39,11 @@ namespace Minesweeper
 
                 Field board = new Field(sideLength);
 
-                board.Draw();
-
                 do
                 {
+                    Console.Clear();
+                    board.Draw();
+
                     Console.WriteLine("You have  options:");
                     Console.WriteLine("  To expose a tile, enter E");
                     Console.WriteLine("  To flag a mine, enter F");
@@ -49,26 +53,27 @@ namespace Minesweeper
 
                     userChoice = Console.ReadLine().ToLower();
 
+                    if (userChoice == "q")
+                    {
+                        Console.Clear();
+                        Console.Write("I see that you cannot handle the minefield. At least you still have your life!");
+                    }
+                    else
+                    {
+                        Console.Write("Please enter two coordinates separated by a space (i.e. Z B) to select a tile: ");
+                        userSpace = Console.ReadLine().ToUpper();
 
+                        //userCoords = userSpace.Split(' ');
+                    }
+
+                    Console.WriteLine("Would you like to play again (y/n)?");
+                    finalAnswer = Console.ReadLine();
                 }
-
                 while (userChoice != "q");
-
-                if (userChoice == "q")
-                {
-                    Console.Clear();
-                    Console.Write("I see that you cannot handle the minefield. At least you still have your life!");
-                }
-                else
-                {
-                    Console.Write("Please enter two coordinates (i.e. ZB) to select a space: ");
-                    userSpace = Console.ReadLine().ToUpper();
-                }
-
-                Console.WriteLine("Would you like to continue (y/n)?");
-                answer = Console.ReadLine();
             }
-            while (answer.ToLower() == "y");
+            while (finalAnswer.ToLower() == "y");
+
+            Console.Write("Goodbye, you can brave the minefield again another day.");
         }
             
         public void Boom()
